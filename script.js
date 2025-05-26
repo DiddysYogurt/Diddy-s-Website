6bec9a273267da20c1652c9969c1376ad9aac8dd
DiddysYogurt <pcjbhc@gmail.com>
2025-05-25 22:50:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-    // List of background images
-    const backgrounds = [
-        "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background1.jpg",
-        "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background2.jpg",
-        "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background3.jpg"
-    ];
+// List of background images
+const backgrounds = [
+    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background1.jpg",
+    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background2.jpg",
+    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background3.jpg"
+];
 
-    // Function to change background
-    function changeBackground() {
-        const body = document.body;
-        const randomIndex = Math.floor(Math.random() * backgrounds.length);
-        body.style.backgroundImage = `url('${backgrounds[randomIndex]}')`;
-    }
+// Function to change background
+function changeBackground() {
+    const body = document.body;
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+    body.style.backgroundImage = `url('${backgrounds[randomIndex]}')`;
+}
 
-    // Automatically change background every 5 seconds
-    setInterval(changeBackground, 5000);
-});
+// Automatically change background every 5 seconds
+setInterval(changeBackground, 5000);
+
