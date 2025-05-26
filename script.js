8e7e64fc0e1bf43da9c4cbfd4b097b523504c62e
DiddysYogurt <pcjbhc@gmail.com>
2025-05-25 20:50:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-const backgrounds = [
-    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/header page.jpg",
-    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background2.jpg",
-    "https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/background3.jpg"
-];
-
-// Function to change background
-function changeBackground() {
-    const body = document.body;
-    const randomIndex = Math.floor(Math.random() * backgrounds.length);
-    body.style.backgroundImage = `url('${backgrounds[randomIndex]}')`;
-    body.style.backgroundSize = "cover"; 
-    body.style.backgroundPosition = "center";
-    body.style.backgroundAttachment = "fixed";
-}
-
-// Ensure a background image is set initially
-changeBackground();
-
-// Change background every 5 seconds
-setInterval(changeBackground, 5000);
+// Set a fixed background image
+document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/DiddysYogurt/Diddy-s-Website/main/images/Header page.jpg')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundAttachment = "fixed";
 
